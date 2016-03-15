2a5b222f839ac01b1c23bd7257fda5c8b82ed07e
Scott Small <scottgsmall@comcast.net>
2016-03-15 14:07:29 -0700

    Initial version

---
+++ Control/Activation/AbstractActivator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Control\Activation;
+
+abstract class AbstractActivator implements ActivatorInterface {
+
+
+
+}
+
+?>
---
+++ Control/Activation/ActivatorInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Control\Activation;
+
+interface ActivatorInterface {
+
+}
+
+?>
