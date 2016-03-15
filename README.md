b179666ca57e8cce8fd7cf33d1698b5f7ee73924
Scott Small <scottgsmall@comcast.net>
2016-03-15 13:10:36 -0700

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Abstract-WordPress-Plugin
-Collection of abstract base classes and libraries upon which to build actual WordPress plugins using OO design and MVC pattern.
+
+Collection of abstract base classes and libraries upon which to build actual 
+WordPress plugins using OO design and MVC pattern.
+
+NOTE: This plugin does not actually do anything by itself! 
---
+++ abstract-wordpress-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ Plugin Name: Abstract Wordpress Plugin
+ Plugin URI:  https://github.com/scottgsmall/Abstract-WordPress-Plugin.git
+ Description: Collection of abstract base classes and libraries upon which to build actual 
+              WordPress plugins using OO design and MVC pattern.
+ Version:     0.1
+ Author:      Scott Small
+ Author URI:  http://rhythmalitic.com
+ */
