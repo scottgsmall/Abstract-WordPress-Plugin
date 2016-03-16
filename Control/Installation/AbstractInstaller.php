a24dd042e67f5911f3246d1a8c25c1183f238db1
Scott Small <scottgsmall@comcast.net>
2016-03-16 10:52:38 -0700

    Initial version

---
+++ Control/Installation/AbstractInstaller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Control\Installation;
+
+abstract class AbstractInstaller implements InstallerInterface {
+
+
+}
+
+?>
---
+++ Control/Installation/InstallerInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Control\Installation;
+
+interface InstallerInterface {
+
+}
+
+?>
